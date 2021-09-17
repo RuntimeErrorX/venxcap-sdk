abb9df9108728043a6abedcc233623fd042b648c
RuntimeErrorX <kholder2@gmail.com>
2021-09-17 17:37:05 -0400

    update token structure

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -6,14 +6,23 @@ import { Currency } from './currency'
 /**
  * Represents an ERC20 token with a unique address and some metadata.
  */
-export class Token extends Currency {
+ export class Token extends Currency {
   public readonly chainId: ChainId
   public readonly address: string
+  public readonly projectLink?: string
 
-  public constructor(chainId: ChainId, address: string, decimals: number, symbol?: string, name?: string) {
+  public constructor(
+    chainId: ChainId,
+    address: string,
+    decimals: number,
+    symbol?: string,
+    name?: string,
+    projectLink?: string
+  ) {
     super(decimals, symbol, name)
     this.chainId = chainId
     this.address = validateAndParseAddress(address)
+    this.projectLink = projectLink
   }
 
   /**
